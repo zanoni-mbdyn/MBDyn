c6d6989fe63964f1c9631ce1b00755416a998cae
Pierangelo Masarati <pierangelo.masarati@polimi.it>
2024-08-02 14:47:17 +0200

    fix

--- modules/module-cosim-output/module-cosim-output.cc
+++ modules/module-cosim-output/module-cosim-output.cc
@@ -45,8 +45,78 @@
 #include "bufmod.h"
 #include "drive_.h"
 
+
+// StreamContentCosim - begin
+
+class StreamContentCosim : public StreamContent {
+protected:
+	const StructNode * pNode;
+
+public:
+	StreamContentCosim(const StructNode *pNode,
+		StreamContent::Modifier *pMod /* not needed */ );
+	virtual ~StreamContentCosim(void);
+
+	void Prepare(void);
+	unsigned GetNumChannels(void) const;
+};
+
+// #include "geomdata.h"
+
+StreamContentCosim::StreamContentCosim(const StructNode * pNode,
+	StreamContent::Modifier *pMod)
+: StreamContent(0, pMod), pNode(pNode)
+{
+	unsigned int size = sizeof(doublereal)*6;
+
+	buf.resize(size);
+	memset(&buf[0], 0, size);
+	m_pMod->Set(size, &buf[0]);
+}
+
+StreamContentCosim::~StreamContentCosim(void)
+{
+	NO_OP;
+}
+
+void
+StreamContentCosim::Prepare(void)
+{
+	doublereal *dbuf = (doublereal *)&buf[0];
+
+	// position
+	const Vec3& X = pNode->GetXCurr();
+
+	// TODO
+	dbuf[0] = X(1);
+	dbuf[1] = X(2);
+	dbuf[2] = X(3);
+
+	// orientation
+	dbuf += 3*sizeof(doublereal);
+
+	const Mat3x3& R = pNode->GetRCurr();
+
+	dbuf[0] = R(1, 1);
+	dbuf[1] = R(1, 2);
+	dbuf[2] = R(1, 3);
+
+	m_pMod->Modify();
+
+	ASSERT(curbuf == &buf[buf.size()]);
+}
+
+unsigned
+StreamContentCosim::GetNumChannels(void) const
+{
+	return buf.size()/sizeof(doublereal);
+}
+
+// StreamContentCosim - end
+
+
 // Read stream output element content type for cosimulation
-struct CosimStreamOutputReader : public StreamOutputContentTypeReader{
+struct CosimStreamOutputReader : public StreamOutputContentTypeReader {
 	virtual StreamContent* Read(DataManager* pDM, MBDynParser& HP);
 };
 
@@ -59,7 +129,7 @@ int module_init(const char *module_name, void *pdm, void *php){
 #endif
 	// Stream output element content type for cosimulation
 	StreamOutputContentTypeReader *rf = new CosimStreamOutputReader;
-	if (!SetStreamOutputContentType("FlightGear", rf)) {
+	if (!SetStreamOutputContentType("cosimulation", rf)) {
 		delete rf;
 		return -1;
 	}
