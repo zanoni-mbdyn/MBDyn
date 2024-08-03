2103d9bc6a73d7e5c08a3dad8a323fba9301cf61
Pierangelo Masarati <pierangelo.masarati@polimi.it>
2024-08-03 11:02:19 +0200

    implement prediction

--- modules/module-cosim-output/module-cosim-output.cc
+++ modules/module-cosim-output/module-cosim-output.cc
@@ -43,17 +43,22 @@
 #include "socketstream_out_elem.h"
 #include "socketstreamdrive.h"
 #include "bufmod.h"
-#include "drive_.h"
+//#include "drive_.h"
+#include "stepsol.h"
 
 
 // StreamContentCosim - begin
 
 class StreamContentCosim : public StreamContent {
 protected:
-	const StructNode * pNode;
+	const StructNode *m_pNode;
+	doublereal m_h;
+	doublereal m_dRho;
+	bool m_bFirst;
 
 public:
 	StreamContentCosim(const StructNode *pNode,
+		doublereal h, doublereal rho,
 		StreamContent::Modifier *pMod /* not needed */ );
 	virtual ~StreamContentCosim(void);
 
@@ -64,10 +69,14 @@ public:
 // #include "geomdata.h"
 
 StreamContentCosim::StreamContentCosim(const StructNode * pNode,
+	doublereal h, doublereal rho,
 	StreamContent::Modifier *pMod)
-: StreamContent(0, pMod), pNode(pNode)
+: StreamContent(0, pMod), m_pNode(pNode), m_h(h), m_dRho(rho), m_bFirst(true)
 {
 	unsigned int size = sizeof(doublereal)*6;
+
+	ASSERT(h > 0.);
+	ASSERT((rho >= 0.) and (rho <= 1.));
 
 	buf.resize(size);
 	memset(&buf[0], 0, size);
@@ -82,31 +91,112 @@ StreamContentCosim::~StreamContentCosim(void)
 void
 StreamContentCosim::Prepare(void)
 {
-	// NOTE: what about prediction?
+	if (m_bFirst) {
+		// at the beginning, nothing is known about the previous step
+		// use Crank Nicolson, extrapolating constant velocity (explicit Euler)
 
-	doublereal *dbuf = (doublereal *)&buf[0];
+		doublereal *dbuf = (doublereal *)&buf[0];
 
-	// position
-	const Vec3& X = pNode->GetXCurr();
+		// predicted position
+		const Vec3& X(m_pNode->GetXCurr());
+		const Vec3& V(m_pNode->GetVCurr());
 
-	// current position; TODO
-	dbuf[0] = X(1);
-	dbuf[1] = X(2);
-	dbuf[2] = X(3);
+		dbuf[0] = X(1) + V(1)*m_h;
+		dbuf[1] = X(2) + V(2)*m_h;
+		dbuf[2] = X(3) + V(3)*m_h;
 
-	// orientation
+		// predicted orientation
+		const Mat3x3& R = m_pNode->GetRCurr();
+		const Vec3& Omega = m_pNode->GetWCurr();
+		Vec3 g_p1(Omega*m_h);
+		Mat3x3 RDelta = Mat3x3(CGR_Rot::MatR, g_p1);
+		Vec3 Theta(RotManip::VecRot(RDelta*R));
 
-	const Mat3x3& R = pNode->GetRCurr();
-	Vec3 Theta(RotManip::VecRot(R));
+		dbuf[3 + 0] = Theta(1);
+		dbuf[3 + 1] = Theta(2);
+		dbuf[3 + 2] = Theta(3);
 
-	// current orientation; TODO
-	dbuf[3 + 0] = Theta(1);
-	dbuf[3 + 1] = Theta(2);
-	dbuf[3 + 2] = Theta(3);
+		// reset flag
+		m_bFirst = false;
+
+	} else {
+
+		// Prediction (using fixed step m_h and linear two-step method with tunable algorithmic dissipation
+		doublereal dAlpha = 1.;
+		doublereal dDen = 2.*(1. + dAlpha) - (1. - m_dRho)*(1. - m_dRho);
+		doublereal dBeta = dAlpha*((1. - m_dRho)*(1. - m_dRho)*(2. + dAlpha)
+			+2.*(2.*m_dRho - 1.)*(1. + dAlpha))/dDen;
+		doublereal dDelta = .5*dAlpha*dAlpha*(1. - m_dRho)*(1. - m_dRho)/dDen;
+
+		doublereal m_mp[2], m_np[2], m_a[StepNIntegrator::IDX_A2 + 1], m_b[StepNIntegrator::IDX_B2 + 1];
+
+		m_mp[0] = -6.*dAlpha*dAlpha*(1. + dAlpha);
+		m_mp[1] = -m_mp[0];
+		m_np[0] = 1. + 4.*dAlpha + 3.*dAlpha*dAlpha;
+		m_np[1] = dAlpha*(2. + 3.*dAlpha);
+
+		m_a[StepNIntegrator::IDX_A1] = 1. - dBeta;
+		m_a[StepNIntegrator::IDX_A2] = dBeta;
+		m_b[StepNIntegrator::IDX_B0] = m_h*(dDelta/dAlpha + dAlpha/2.);
+		m_b[StepNIntegrator::IDX_B1] = m_h*(dBeta/2. + dAlpha/2. - dDelta/dAlpha*(1. + dAlpha));
+		m_b[StepNIntegrator::IDX_B2] = m_h*(dBeta/2. + dDelta);
+
+		m_mp[0] /= m_h;
+		m_mp[1] /= m_h;
+
+		doublereal *dbuf = (doublereal *)&buf[0];
+
+		// predicted position
+		const Vec3& X = m_pNode->GetXCurr();
+		const Vec3& X_m1 = m_pNode->GetXPrev();
+		const Vec3& V = m_pNode->GetVCurr();
+		const Vec3& V_m1 = m_pNode->GetVPrev();
+
+		for (integer idx = 0; idx < 3; ++idx) {
+			// predicted velocity component (derivative of cubic extrapolation on position)
+			doublereal dx_p1 = m_mp[0]*X(idx + 1) + m_mp[1]*X_m1(idx + 1) + m_np[0]*V(idx + 1) + m_np[1]*V_m1(idx + 1);
+
+			// predicted position (two-step linear multistep method with algorithmic dissipation)
+			dbuf[idx] = m_a[StepNIntegrator::IDX_A1]*X(idx + 1) + m_a[StepNIntegrator::IDX_A2]*X_m1(idx + 1)
+				+ m_b[StepNIntegrator::IDX_B0]*dx_p1
+				+ m_b[StepNIntegrator::IDX_B1]*V(idx + 1) + m_b[StepNIntegrator::IDX_B2]*V_m1(idx + 1);
+		}
+
+		// predicted orientation
+		const Mat3x3& R = m_pNode->GetRCurr();
+		const Mat3x3& R_m1 = m_pNode->GetRPrev();
+		const Vec3& Omega = m_pNode->GetWCurr();
+		const Vec3& Omega_m1 = m_pNode->GetWPrev();
+
+		// backward relative rotation between current and previous step
+		Mat3x3 RDelta(R_m1.MulMT(R));
+		// assuming g == 0
+		Vec3 g_m1(CGR_Rot::Param, RDelta);
+		Vec3 gP_m1(Mat3x3(CGR_Rot::MatGm1, g_m1)*Omega_m1);
+		const Vec3& gP(Omega);
+
+		Vec3 g_p1;
+		for (integer idx = 0; idx < 3; ++idx) {
+			// predicted orientation parameter derivative component (derivative of cubic extrapolation on position)
+			doublereal dg_p1 = /* m_mp[0]*g(idx + 1) + */ m_mp[1]*g_m1(idx + 1) + m_np[0]*gP(idx + 1) + m_np[1]*gP_m1(idx + 1);
+
+			// predicted orientation parameter (two-step linear multistep method with algorithmic dissipation)
+			g_p1(idx + 1) = /* m_a[StepNIntegrator::IDX_A1]*g(idx + 1) + */ m_a[StepNIntegrator::IDX_A2]*g_m1(idx + 1)
+				+ m_b[StepNIntegrator::IDX_B0]*dg_p1
+				+ m_b[StepNIntegrator::IDX_B1]*gP(idx + 1) + m_b[StepNIntegrator::IDX_B2]*gP_m1(idx + 1);
+		}
+
+		// forward relative rotation between current and next step
+		RDelta = Mat3x3(CGR_Rot::MatR, g_p1);
+		// absolute Euler vector of predicted orientation
+		Vec3 Theta(RotManip::VecRot(RDelta*R));
+
+		dbuf[3 + 0] = Theta(1);
+		dbuf[3 + 1] = Theta(2);
+		dbuf[3 + 2] = Theta(3);
+	}
 
 	m_pMod->Modify();
-
-	ASSERT(curbuf == &buf[buf.size()]);
 }
 
 unsigned
@@ -130,9 +220,42 @@ CosimStreamOutputReader::Read(DataManager* pDM, MBDynParser& HP)
 
 	const StructNode* pNode = pDM->ReadNode<const StructNode, Node::STRUCTURAL>(HP);
 
+	doublereal h(-1.);
+	if (HP.IsKeyWord("time" "step")) {
+		try {
+			h = HP.GetReal(0., HighParser::range_gt<doublereal>(0.));
+
+		} catch (HighParser::ErrValueOutOfRange<doublereal>& e) {
+			silent_cerr("error: invalid reference time step" << e.Get() << " (must be positive) [" << e.what() << "] for CosimStreamOutput at line " << HP.GetLineData() << std::endl);
+			throw e;
+		}
+
+	} else {
+		silent_cerr("time step required for CosimStreamOutput at line " << HP.GetLineData() << std::endl);
+		throw DataManager::ErrGeneric(MBDYN_EXCEPT_ARGS);
+	}
+
+	doublereal rho(0.);
+	if (HP.IsKeyWord("bdf") || HP.IsKeyWord("bdf2")) {
+		// BDF2, the default
+		rho = 0.;
+
+	} else if (HP.IsKeyWord("rho")) {
+		try {
+			rho = HP.GetReal(0., HighParser::range_ge_le<doublereal>(0., 1.));
+
+		} catch (HighParser::ErrValueOutOfRange<doublereal>& e) {
+			silent_cerr("error: invalid reference asymptotic spectral radius " << e.Get() << " (must be greater than or equal to zero and lower than or equal to one) [" << e.what() << "] for CosimStreamOutput at line " << HP.GetLineData() << std::endl);
+			throw e;
+		}
+
+	} else {
+		silent_cout("Warning, no spectral radius (rho) provided; using 0 (BDF2) at line " << HP.GetLineData() << std::endl);
+	}
+
 	StreamContent::Modifier *pMod(0);
 
-	SAFENEWWITHCONSTRUCTOR(pSC, StreamContentCosim, StreamContentCosim(pNode, pMod));
+	SAFENEWWITHCONSTRUCTOR(pSC, StreamContentCosim, StreamContentCosim(pNode, h, rho, pMod));
 
 	return pSC;
 }
