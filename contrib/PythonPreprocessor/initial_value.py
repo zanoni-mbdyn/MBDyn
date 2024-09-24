fea7b0968f51cfe123f75f1e19da7c07aea692de
Shimul-Baidya <shimul.cse31@gmail.com>
2024-09-24 23:24:20 +0600

    Added MaxIterations and Method classes in initial_value.py

--- contrib/PythonPreprocessor/initial_value.py
+++ contrib/PythonPreprocessor/initial_value.py
@@ -63,11 +63,11 @@ class Strategy(MBEntity):
 
 class StrategyFactor(Strategy):
     reduction_factor: Union[float, MBVar]
-    steps_before_reduction: Union[float, MBVar]
+    steps_before_reduction: Union[int, MBVar]
     raise_factor: Union[float, MBVar]
-    steps_before_raise: Union[float, MBVar]
-    min_iterations: Union[float, MBVar]
-    max_iterations: Optional[Union[float, MBVar]] = None
+    steps_before_raise: Union[int, MBVar]
+    min_iterations: Union[int, MBVar]
+    max_iterations: Optional[Union[int, MBVar]] = None
 
     def __str__(self):
         s = f'{self.strategy_header()}, {self.steps_before_reduction}'
@@ -120,6 +120,95 @@ class Tolerance(MBEntity):
             if self.solution_test is not None:
                 s += f', test, {self.solution_test}'
 
+class MaxIterations(MBEntity):
+    '''Error out after max_iterations without passing the convergence test. The default value is zero.'''
+
+    max_iterations: int = 0
+    optional_keywords: Optional[Literal['at most']] = None
+
+    def __str__(self):
+        s = f'max iterations: {self.max_iterations}'
+        if self.optional_keywords is not None:
+            s += f', {self.optional_keywords}'
+        return s
+
+class Method(MBEntity):
+    """Base class for every methods"""
+
+    @abstractmethod
+    def __str__(self) -> str:
+        """Has to be overridden to output the MBDyn syntax"""
+        pass
+
+class CrankNikson(Method):
+    def __str__(self):
+        s = 'method: crank nikson'
+        return s
+
+class MS(Method):
+    '''
+    The 'ms' method is proved to be more accurate at high values of asymptotic radius (low dissipation),
+    while the 'hope' method is proved to be more accurate at low values of the radius (high dissipation). They look
+    nearly equivalent at radii close to 0.4, with the former giving the best compromise between algorithmic
+    dissipation and accuracy at about 0.6.
+    '''
+
+    differential_radius: Union[DriveCaller, DriveCaller2]
+    algebraic_radius: Optional[Union[DriveCaller, DriveCaller2]] = None
+
+    def __str__(self):
+        s = f'method: ms, {self.differential_radius}'
+        if self.algebraic_radius is not None:
+            s += f', {self.algebraic_radius}'
+        return s
+
+class Hope(Method):
+    '''
+    The 'ms' method is proved to be more accurate at high values of asymptotic radius (low dissipation),
+    while the 'hope' method is proved to be more accurate at low values of the radius (high dissipation). They look
+    nearly equivalent at radii close to 0.4, with the former giving the best compromise between algorithmic
+    dissipation and accuracy at about 0.6.
+    '''
+
+    differential_radius: Union[DriveCaller, DriveCaller2]
+    algebraic_radius: Optional[Union[DriveCaller, DriveCaller2]] = None
+
+    def __str__(self):
+        s = f'method: hope, {self.differential_radius}'
+        if self.algebraic_radius is not None:
+            s += f', {self.algebraic_radius}'
+        return s
+    
+class ThirdOrder(Method):
+    '''
+    This method is experimental. It is a third-order, two stage unconditionally stable method,
+    which can be tuned to give the desired algorithmic dissipation by setting the value of the asymptotic
+    spectral radius, which should not be too close to zero. Currently it is not possible to independently set
+    the radius for the differential and the algebraic variables.
+    '''
+
+    differential_radius: Union[DriveCaller, DriveCaller2, Literal['ad hoc']]
+
+    def __str__(self):
+        s = f'method: third order, {self.differential_radius}'
+        return s
+    
+class BFD(Method):
+    order: Optional[Union[int, MBVar]] = None # 1 / 2
+    '''only first order (implicit Euler) and second order formulas are currently implemented, and the
+    default is the second order formula, which is the most useful'''
+
+    def __str__(self):
+        s = 'method: bfd'
+        if self.order is not None:
+            s += f', order, {self.order}'
+        return s
+
+class ImplicitEuler(Method):
+    def __str__(self):
+        s = 'method: implicit euler'
+        return s
+
 class InitialValue(MBEntity):
     '''
     At present, the main problem is initial value, which solves initial value problems by means of generic
@@ -134,4 +223,18 @@ class InitialValue(MBEntity):
     max_time_step: Optional[Union[float, MBVar]] = None
     time_step: Union[float, MBVar]
     tolerance: Tolerance
+    max_iterations: MaxIterations
+    modify_residual_test: Optional[Union[bool, int]] = False    # 0 / 1 / True / False
+    method: Method
+
+    @field_validator('modify_residual_test', mode='after')
+    def set_modify_residual_test(cls, v):
+        if isinstance(v, int) or isinstance(v, bool):
+            if v == 0 or v == False:
+                return None
+            elif v == 1 or v == True:
+                return "modify residual test"
+            else:
+                raise ValueError("modify_residual_test has to be an int: 0 / 1, or a bool: True / False")
+ 
 
