a2f79ec70087ce3a4c3aac4b9fefd7c6936aceb2
Marco Morandini <marco.morandini@polimi.it>
2026-02-27 18:25:06 +0100

    tpetraspmh firts attempt

--- libraries/libmbmath/tpetraspmh.h
+++ libraries/libmbmath/tpetraspmh.h
@@ -29,37 +29,33 @@
  */
 
 /*
- AUTHOR: Reinhard Resch <mbdyn-user@a1.net>
-        Copyright (C) 2022(-2023) all rights reserved.
+  AUTHOR: Reinhard Resch <mbdyn-user@a1.net>
+  Copyright (C) 2022(-2023) all rights reserved.
 
-        The copyright of this code is transferred
-        to Pierangelo Masarati and Paolo Mantegazza
-        for use in the software MBDyn as described
-        in the GNU Public License version 2.1
+  The copyright of this code is transferred
+  to Pierangelo Masarati and Paolo Mantegazza
+  for use in the software MBDyn as described
+  in the GNU Public License version 2.1
+
+  Tpetra port: converted from Epetra by the MBDyn project.
 */
 
-#ifndef __EPETRA_SPARSE_MATRIX_HANDLER_H__INCLUDED__
-#define __EPETRA_SPARSE_MATRIX_HANDLER_H__INCLUDED__
+#ifndef __TPETRA_SPARSE_MATRIX_HANDLER_H__INCLUDED__
+#define __TPETRA_SPARSE_MATRIX_HANDLER_H__INCLUDED__
 
 #ifdef USE_TRILINOS
 #include "myassert.h"
 #include "spmh.h"
 #include "cscmhtpl.h"
+#include "tpetra_types.h"
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wcpp"
-#include <Epetra_Comm.h>
-#include <Epetra_Map.h>
-#include <Epetra_CrsMatrix.h>
-#pragma GCC diagnostic pop
-
-class EpetraSparseMatrixHandler: public SparseMatrixHandler {
+class TpetraSparseMatrixHandler: public SparseMatrixHandler {
 public:
-     EpetraSparseMatrixHandler(const integer& iNumRows,
+     TpetraSparseMatrixHandler(const integer& iNumRows,
                                const integer& iNumCols,
                                integer iNumColsAlloc,
-                               const Epetra_Comm& oComm);
-     virtual ~EpetraSparseMatrixHandler();
+                               const Teuchos::RCP<const TpetraComm>& pComm);
+     virtual ~TpetraSparseMatrixHandler();
 
 #ifdef DEBUG
      virtual void IsValid() const override;
@@ -132,29 +128,37 @@ public:
      VectorHandler& GetCol(integer icol,
                            VectorHandler& out) const override;
 
-     virtual void Scale(const std::vector<doublereal>& oRowScale, const std::vector<doublereal>& oColScale) override;
+     virtual void Scale(const std::vector<doublereal>& oRowScale,
+                        const std::vector<doublereal>& oColScale) override;
 
-     virtual bool AddItem(integer iRow, const sp_grad::SpGradient& oItem) override;
+     virtual bool AddItem(integer iRow,
+                          const sp_grad::SpGradient& oItem) override;
 
      virtual void
-     IncCoef(integer iRow, integer iCol, const doublereal& dCoef) override final;
+     IncCoef(integer iRow, integer iCol,
+             const doublereal& dCoef) override final;
 
      virtual void
-     DecCoef(integer iRow, integer iCol, const doublereal& dCoef) override final;
-     
-     virtual void EnumerateNz(const std::function<EnumerateNzCallback>& func) const override;
+     DecCoef(integer iRow, integer iCol,
+             const doublereal& dCoef) override final;
+
+     virtual void EnumerateNz(
+          const std::function<EnumerateNzCallback>& func) const override;
 
      virtual doublereal Norm(Norm_t eNorm = NORM_1) const override;
 
      virtual integer Nz() const override;
 
-     virtual EpetraSparseMatrixHandler* Copy() const override;
+     virtual TpetraSparseMatrixHandler* Copy() const override;
 
      virtual integer PacMat() override;
 
+     /* ---------------------------------------------------------------
+      * Iterator over non-zeros (same interface as the Epetra version)
+      * --------------------------------------------------------------- */
      class const_iterator {
-          friend class EpetraSparseMatrixHandler;
-          
+          friend class TpetraSparseMatrixHandler;
+
           const integer* const rowptr;
           const integer* const colind;
           const doublereal* const values;
@@ -162,45 +166,43 @@ public:
           const integer NRows;
 #endif
           const integer NZ;
-	  integer iIdx;
-	  SparseMatrixHandler::SparseMatrixElement elem;
-          
-#ifdef DEBUG
-	  bool bInvariant() const {
-	       ASSERT(iIdx >= 0);
-	       ASSERT(iIdx <= NZ);
-	       ASSERT(iIdx == NZ || (elem.iRow >= 0 && elem.iRow < NRows));
-	       ASSERT(iIdx == NZ || rowptr[elem.iRow] <= iIdx);
-	       ASSERT(iIdx == NZ || rowptr[elem.iRow + 1] > iIdx);
-               
-	       return true;
-	  }
-#endif
-	  void UpdateElem() {
-	       if (iIdx < NZ) {
-		    elem.iCol = colind[iIdx];
-		    elem.dCoef = values[iIdx];
+          integer iIdx;
+          SparseMatrixHandler::SparseMatrixElement elem;
 
-		    if (iIdx >= rowptr[elem.iRow + 1]) {
-			 ++elem.iRow;
-		    }
-	       } else {
-		    ASSERT(iIdx == NZ);
 #ifdef DEBUG
-		    elem.iRow = std::numeric_limits<decltype(elem.iRow)>::min();
-		    elem.iCol = std::numeric_limits<decltype(elem.iCol)>::min();
-		    elem.dCoef = -std::numeric_limits<decltype(elem.dCoef)>::max();
+          bool bInvariant() const {
+               ASSERT(iIdx >= 0);
+               ASSERT(iIdx <= NZ);
+               ASSERT(iIdx == NZ || (elem.iRow >= 0 && elem.iRow < NRows));
+               ASSERT(iIdx == NZ || rowptr[elem.iRow] <= iIdx);
+               ASSERT(iIdx == NZ || rowptr[elem.iRow + 1] > iIdx);
+               return true;
+          }
 #endif
-	       }
-	  }
+          void UpdateElem() {
+               if (iIdx < NZ) {
+                    elem.iCol  = colind[iIdx];
+                    elem.dCoef = values[iIdx];
+                    if (iIdx >= rowptr[elem.iRow + 1]) {
+                         ++elem.iRow;
+                    }
+               } else {
+                    ASSERT(iIdx == NZ);
+#ifdef DEBUG
+                    elem.iRow  = std::numeric_limits<decltype(elem.iRow)>::min();
+                    elem.iCol  = std::numeric_limits<decltype(elem.iCol)>::min();
+                    elem.dCoef = -std::numeric_limits<decltype(elem.dCoef)>::max();
+#endif
+               }
+          }
 
-	  const_iterator(const integer* rowptr_a,
+          const_iterator(const integer* rowptr_a,
                          const integer* colind_a,
                          const doublereal* values_a,
-                         integer NRows_a,                         
+                         integer NRows_a,
                          integer iIdx_a,
                          integer iRow)
-	       :rowptr(rowptr_a),
+               :rowptr(rowptr_a),
                 colind(colind_a),
                 values(values_a),
 #ifdef DEBUG
@@ -208,106 +210,119 @@ public:
 #endif
                 NZ(rowptr[NRows_a] - rowptr_a[0]),
                 iIdx(iIdx_a) {
-
-	       elem.iRow = iRow;
-
-	       UpdateElem();
-
-	       ASSERT(bInvariant());
-	  }
+               elem.iRow = iRow;
+               UpdateElem();
+               ASSERT(bInvariant());
+          }
 
      public:
-	  ~const_iterator() {
-	       ASSERT(bInvariant());
-	  }
+          ~const_iterator() { ASSERT(bInvariant()); }
 
-	  const const_iterator& operator++() {
-	       ASSERT(bInvariant());
+          const const_iterator& operator++() {
+               ASSERT(bInvariant());
+               ++iIdx;
+               UpdateElem();
+               ASSERT(bInvariant());
+               return *this;
+          }
 
-	       ++iIdx;
+          const SparseMatrixHandler::SparseMatrixElement* operator->() const {
+               ASSERT(bInvariant());
+               return &elem;
+          }
 
-	       UpdateElem();
+          const SparseMatrixHandler::SparseMatrixElement& operator*() const {
+               ASSERT(bInvariant());
+               return elem;
+          }
 
-	       ASSERT(bInvariant());
-
-	       return *this;
-	  }
-
-	  const SparseMatrixHandler::SparseMatrixElement* operator->() const {
-	       ASSERT(bInvariant());
-
-	       return &elem;
-	  }
-
-	  const SparseMatrixHandler::SparseMatrixElement& operator*() const {
-	       ASSERT(bInvariant());
-
-	       return elem;
-	  }
-
-	  bool operator == (const const_iterator& op) const {
-	       ASSERT(bInvariant());
-	       ASSERT(rowptr == op.rowptr);
+          bool operator==(const const_iterator& op) const {
+               ASSERT(bInvariant());
+               ASSERT(rowptr == op.rowptr);
                ASSERT(colind == op.colind);
                ASSERT(values == op.values);
+               return iIdx == op.iIdx;
+          }
 
-	       return iIdx == op.iIdx;
-	  }
-
-	  bool operator != (const const_iterator& op) const {
-	       ASSERT(bInvariant());
-	       ASSERT(rowptr == op.rowptr);
+          bool operator!=(const const_iterator& op) const {
+               ASSERT(bInvariant());
+               ASSERT(rowptr == op.rowptr);
                ASSERT(colind == op.colind);
                ASSERT(values == op.values);
-               
-	       return iIdx != op.iIdx;
-	  }
+               return iIdx != op.iIdx;
+          }
      };
 
      const_iterator begin() const;
      const_iterator end() const;
 
-     const Epetra_CrsMatrix* pGetEpetraCrsMatrix() const {
-          return &oEPM;
-     }
+     Teuchos::RCP<const TpetraCrs> pGetTpetraCrsMatrix() const { return pMat; }
+     Teuchos::RCP<TpetraCrs>       pGetTpetraCrsMatrix()       { return pMat; }
 
-     Epetra_CrsMatrix* pGetEpetraCrsMatrix() {
-          return &oEPM;
-     }
      using MatrixHandler::operator=;
+
 protected:
      virtual VectorHandler&
      MatVecMul_base(void (VectorHandler::*op)(integer iRow,
                                               const doublereal& dCoef),
-                    VectorHandler& out, const VectorHandler& in) const override;
+                    VectorHandler& out,
+                    const VectorHandler& in) const override;
+
      virtual VectorHandler&
      MatTVecMul_base(void (VectorHandler::*op)(integer iRow,
                                                const doublereal& dCoef),
-                     VectorHandler& out, const VectorHandler& in) const override;
+                     VectorHandler& out,
+                     const VectorHandler& in) const override;
+
 private:
-     void ExtractCrsDataPointers(integer*& rowptr, integer*& colind, doublereal*& values) const;
+     /*
+      * After fillComplete() the matrix data lives in a Kokkos device view.
+      * We mirror it to host once (in PacMat / EnsureFilled) and cache
+      * raw pointers for the iterator and CRS-form extraction.
+      */
+     void EnsureFilled() const;
+     void InsertOrSumValues(TpetraGO globalRow,
+                            integer nEntries,
+                            const doublereal* vals,
+                            const TpetraGO* cols);
+
      CSCMatrixHandlerTpl<doublereal, integer, 0>& GetTransposedCSC() const;
-     
+
      template <typename idx_type>
      idx_type MakeCompressedRowFormTpl(doublereal *const Ax,
-                                          idx_type *const Ai,
-                                          idx_type *const Ap,
-                                          int offset) const;
+                                       idx_type *const Ai,
+                                       idx_type *const Ap,
+                                       int offset) const;
 
      template <typename idx_type>
      idx_type MakeCompressedColumnFormTpl(doublereal *const Ax,
                                           idx_type *const Ai,
                                           idx_type *const Ap,
                                           int offset) const;
-     
+
      inline integer PacMat() const {
-          return const_cast<EpetraSparseMatrixHandler*>(this)->PacMat();
+          return const_cast<TpetraSparseMatrixHandler*>(this)->PacMat();
      }
-     
-     mutable Epetra_CrsMatrix oEPM;
+
+     Teuchos::RCP<const TpetraComm>  pComm;
+     Teuchos::RCP<const TpetraMap>   pRowMap;
+     Teuchos::RCP<const TpetraMap>   pColMap;
+     mutable Teuchos::RCP<TpetraCrs> pMat;
+
      const integer iNumColsAlloc;
+     mutable bool bFilled;
+
+     /*
+      * Host-side CRS arrays populated once after fillComplete().
+      * These back the const_iterator and the MakeCompressed*Form methods
+      * without further device round-trips.
+      */
+     mutable std::vector<integer>    oRowPtr;   /* length NRows+1 */
+     mutable std::vector<integer>    oColInd;   /* length Nz      */
+     mutable std::vector<doublereal> oValues;   /* length Nz      */
+
      mutable CSCMatrixHandlerTpl<doublereal, integer, 0> oCscT;
 };
 
-#endif
-#endif
+#endif  /* USE_TRILINOS */
+#endif  /* __TPETRA_SPARSE_MATRIX_HANDLER_H__INCLUDED__ */
